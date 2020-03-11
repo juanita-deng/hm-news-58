df939bbc3c374b214d8b59e1d691fd0a5ee9a277
juanita <1182926231@qq.com>
2020-03-11 19:36:53 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';//引入根组件
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;//去掉控制提示信息
+// const vm = new Vue({
+//   el:'#app',
+//   render:function(createElement) {
+//     return createElement(App);
+//   }
+// })
+//写法二
 new Vue({
-  router,
-  render: h => h(App)
+  render:c => c(App)
 }).$mount('#app')
